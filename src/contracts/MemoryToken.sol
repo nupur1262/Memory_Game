8df5195e67a9a524580182305f06e91edd038a71
unknown <nupurpahuja09@gmail.com>
2021-05-28 04:01:03 +0530

    initial commit

--- src/contracts/MemoryToken.sol
+++ src/contracts/MemoryToken.sol
@@ -2,7 +2,49 @@ pragma solidity ^0.5.0;
 
 import "./ERC721Full.sol";
 
-contract MemoryToken is ERC721Full {
+contract MemoryToken is ERC721Full{
+  //string public name = "Memory Token";
+
+  //The ERC721 has a constructor , its just the function 
+  //that gets run whenever the smart contract is created 
+  //or put up on the blockchain.
+  //2 parameters are passes , 1st is the name of the token
+  //2nd is the symbol of the token.
+
+  constructor() ERC721Full("Memory Token" , "MEMORY") public{
+
+  }
+
+  //we want to build up the token so that it can be minted
+  //and can be distributed to other ppl
+  //so whenever we are creating the token , 
+  //we want it to start from zero 
+  //whenever the token is created there are no token in existance
+  //and we want ppl to collect them
+  //so for all this logic we will have the mint function.
+
+  //it accepts two arguments
+  //first is the address of the person on the blockchain 
+  //that we are going to give the token to
+  //second is the string that is the tokenURI
+  //every token has some image associated with it 
+  //all these images are stored somewhere on a web server
+  //if we get more decentralized we can store these images on a distributed system like ipfs
+  //so that is the place what a URI is.
+
+  function mint(address _to , string memory _tokenURI) public returns(bool){
+    //totalSupply() is a function in ERC721Full , that returns the number of tokens that already exist
+    //so basically we can assume it to be the index number.
+    //everytime we create a new token , we increment totalId by 1
+    uint _tokenId = totalSupply().add(1);
+    _mint(_to , _tokenId);
+
+    //next we want to set up the tokenURI
+    //there is a function _setTokenURI() in the ERC721 file , which takes the tokenId and a string URI and then sets the uri for the token
+    _setTokenURI(_tokenId , _tokenURI);
+
+    return true;
+  }
   // Code goes here...
 
 }
--- test/MemoryToken.test.js
+++ test/MemoryToken.test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai')
+
 const MemoryToken = artifacts.require('./MemoryToken.sol')
 
 require('chai')
@@ -5,5 +7,79 @@ require('chai')
   .should()
 
 contract('Memory Token', (accounts) => {
+  let token
+
+  //testing framework , special function before
+  //so that we can fetch the token from the blockchain first 
+  //and then do all the stuff with the token
+  before(async()=>{
+    //we want to check is the token is created or not
+    token = await MemoryToken.deployed()
+  })
+
+
+  //This is the description of the test to check if the contract is deployed properly or not
+  describe('Deployment' , async()=>{
+    it('Deployes Successfully' , async()=>{
+      //checking the address of the token created
+      const addres = token.address
+      //check if the address is not null
+      assert.notEqual(addres,0x0)
+      assert.notEqual(addres,'')
+      assert.notEqual(addres,null)
+      assert.notEqual(addres,undefined)
+    })
+  
+    //check the name 
+    it('Has a name',async()=>{
+      const name = await token.name()
+      assert.equal(name , 'Memory Token')
+    })
+  
+    //check the symbol
+    it('Has a Symbol' , async()=>{
+      const sym = await token.symbol()
+      assert.equal(sym , "MEMORY")
+    })
+  })
+  
+
+
+  //This is the description to check if the Token distribution is done correctly not
+  describe('Token Distribution' , async()=>{
+    let result
+
+    it('Mints Tokens' , async()=>{
+      await token.mint(accounts[0] , 'https://www.token-uri.com/nft')
+
+      //it should increase the total supply
+      result = await token.totalSupply()
+      assert.equal(result.toString() , '1' ,'Total Supply is correct')
+
+      //check that the total balance of the owner is increased
+      result = await token.balanceOf(accounts[0])
+      assert.equal(result.toString() , '1' ,'balanceOf() is correct')
+
+      //make sure that the token belongs to the owner 
+      result = await token.ownerOf('1')
+      assert.equal(result.toString(), accounts[0].toString() , 'ownerOf() is correct')
+      result = await token.tokenOfOwnerByIndex(accounts[0] , 0)
+      //Owner can see all the tokens
+      let balanceOf = await token.balanceOf(accounts[0])
+      let tokenIds=[]
+      for(let i=0;i<balanceOf;i++)
+      {
+        let id = await token.tokenOfOwnerByIndex(accounts[0] , i)
+          tokenIds.push(id.toString())
+      }
+      let expected = ['1']
+      assert.equal(tokenIds.toString() , expected.toString() ,'Token Ids are correct')
+
+      //Token URI is correct
+      let tokenURI = await token.tokenURI('1')
+      assert.equal(tokenURI , 'https://www.token-uri.com/nft')
+
+      })
+  })
   // code goes here...
 })
